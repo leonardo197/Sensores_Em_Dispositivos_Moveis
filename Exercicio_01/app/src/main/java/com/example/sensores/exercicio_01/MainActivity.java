0cb6da0ba611041704163937c8d0be687b0256fb
Fábio Lameira <36962813+fabiolameira97@users.noreply.github.com>
2018-05-16 16:43:20 +0100

    Final Update

--- Exercicio_01/app/src/main/java/com/example/sensores/exercicio_01/MainActivity.java
+++ Exercicio_01/app/src/main/java/com/example/sensores/exercicio_01/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.sensores.exercicio_01;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ListView listView;
+    private SensorManager mSensorManager;
+    private List<Sensor> deviceSensors = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = ((ListView) findViewById(R.id.listView));
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        listView.setAdapter(new ArrayAdapter<Sensor>(this, android.R.layout.simple_list_item_1, deviceSensors));
+
     }
 }
