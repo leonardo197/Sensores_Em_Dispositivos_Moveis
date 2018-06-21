332f4fdb76d38d68f9be5983ef1ff2c8b381f4f9
leonardo197 <leonardocosta197@gmail.com>
2018-06-21 14:42:01 +0100

    erg

--- projeto_finalGPS/app/src/main/java/com/example/sensores/projeto_finalgps/MainActivity.java
+++ projeto_finalGPS/app/src/main/java/com/example/sensores/projeto_finalgps/MainActivity.java
@@ -5,15 +5,18 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,41 +31,64 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txLongitude=(TextView)findViewById(R.id.txLongitude);
-        txLatitude=(TextView)findViewById(R.id.txLatitude);
-
+        
+        txLongitude = (TextView) findViewById(R.id.txLongitude);
+        txLatitude = (TextView) findViewById(R.id.txLatitude);
 
 
 //vwrificar se o gps esta premitido(obrigatorio)
-      if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-          localionM=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
-          cordenadas=localionM.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        }else{localionM=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
-          cordenadas=localionM.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //localionM=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            //cordenadas=localionM.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        } else {
+            localionM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            cordenadas = localionM.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
 
-       double Longitude=cordenadas.getLongitude();
-        double Latitude=cordenadas.getLatitude();
+            double Longitude = cordenadas.getLongitude();
+            double Latitude = cordenadas.getLatitude();
 
-        txLatitude.setText(""+Longitude+" "+cordenadas.getSpeed());
-        txLongitude.setText(""+Latitude);
-
-
+            txLatitude.setText("" + Longitude);
+            txLongitude.setText("" + Latitude);
 
 
-          File arquivo = new File("teste.txt");
-          try{
-              FileWriter fw = new FileWriter(arquivo);
-              fw.write('2');
-              fw.write(Longitude+" "+Longitude);
-              fw.flush();
-          }catch(IOException ex){
-              txLongitude.setText("ola");
-          }
+///////////////////////////////////////////////////////////////////
+            String nome_do_arquivo = "teste.csv";
+            String texto_do_arquivo = "ola ,1,234,9,";
+            try {
+                File root = new File(Environment.getExternalStorageDirectory(), "Download");
+                if (!root.exists()) {
+                    root.mkdirs();
+                }
+                File gpxfile = new File(root,nome_do_arquivo);
+                FileWriter writer= new FileWriter(gpxfile);
 
+                writer.append("DisplayName");
+                writer.append(',');
+                writer.append("Age");
+                writer.append('\n');
 
+                writer.append("MKYONG");
+                writer.append(',');
+                writer.append("26");
+                writer.append('\n');
+
+                writer.append("YOUR NAME");
+                writer.append(',');
+                writer.append("29");
+                writer.append('\n');
+                writer.append(texto_do_arquivo);
+                writer.flush();
+                writer.close();
+                Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+
+                Toast.makeText(this, "nao", Toast.LENGTH_SHORT).show();
+            }
+
+        }
 
     }
 
-    }
+
 }
