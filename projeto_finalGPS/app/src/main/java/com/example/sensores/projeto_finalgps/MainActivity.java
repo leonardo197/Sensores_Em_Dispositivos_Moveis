0ce636a77f6cfd075ea916c9d8091b41175dd329
leonardo197 <leonardocosta197@gmail.com>
2018-06-22 11:11:05 +0100

    ja grava os passos e cordenadas

--- projeto_finalGPS/app/src/main/java/com/example/sensores/projeto_finalgps/MainActivity.java
+++ projeto_finalGPS/app/src/main/java/com/example/sensores/projeto_finalgps/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sensores.projeto_finalgps;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Environment;
@@ -10,6 +14,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,76 +25,166 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 
-public class MainActivity extends AppCompatActivity {
-    public TextView txLongitude;
-    public TextView txLatitude;
-
-    public Location cordenadas;
-    public LocationManager localionM;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    TextView txLongitude;
+    TextView txLatitude;
+    TextView textViewCounter;
+    Location cordenadas;
+    LocationManager localionM;
+    SensorManager sensorManager;
+    public float passos = 0, t;
+    Button btnStart;
+    Button btnStop;
+    boolean contador_de_passos = false;
+    boolean gravar = false;
+    public double Longitude;
+    public double Latitude;
+    String texto_do_arquivo;
+    File root;
+    public FileWriter writer;
+    double X = 0, Z = 0, Y = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         txLongitude = (TextView) findViewById(R.id.txLongitude);
         txLatitude = (TextView) findViewById(R.id.txLatitude);
+        textViewCounter = (TextView) findViewById(R.id.textViewCounter);
+
+        btnStart = (Button) findViewById(R.id.buttonStart);
+        btnStop = (Button) findViewById(R.id.buttonStop);
 
 
-//vwrificar se o gps esta premitido(obrigatorio)
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            //localionM=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
-            //cordenadas=localionM.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        } else {
-            localionM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            cordenadas = localionM.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        btnStart.setOnClickListener(
+                new View.OnClickListener() {
 
+                    @Override
+                    public void onClick(View view) {
+                        passos = 0;
+                        String nome_do_arquivo = "teste.csv";
+                        textViewCounter.setText(String.valueOf(passos));
+                        root = new File(Environment.getExternalStorageDirectory(), "Download");
+                        if (!root.exists()) {
+                            root.mkdirs();
+                        }
+                        File gpxfile = new File(root, nome_do_arquivo);
+                        try {
+                            writer = new FileWriter(gpxfile);
+                            Toast.makeText(getApplicationContext(), "a gravar", Toast.LENGTH_SHORT).show();
+                            writer.append("Longitude;Latitude;Pedometro;Posicao X;Posicao Y;Posicao Z");
+                            writer.append('\n');
+                            gravar = true;
 
-            double Longitude = cordenadas.getLongitude();
-            double Latitude = cordenadas.getLatitude();
+                        } catch (IOException e) {
 
-            txLatitude.setText("" + Longitude);
-            txLongitude.setText("" + Latitude);
-
-
-///////////////////////////////////////////////////////////////////
-            String nome_do_arquivo = "teste.csv";
-            String texto_do_arquivo = "ola ,1,234,9,";
-            try {
-                File root = new File(Environment.getExternalStorageDirectory(), "Download");
-                if (!root.exists()) {
-                    root.mkdirs();
+                            Toast.makeText(getApplicationContext(), "n'Saved", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    writer.flush();
+                    writer.close();
+                    Toast.makeText(getApplicationContext(), "ficheiro guardado", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "erro na a gravar ficheiro", Toast.LENGTH_SHORT).show();
                 }
-                File gpxfile = new File(root,nome_do_arquivo);
-                FileWriter writer= new FileWriter(gpxfile);
-
-                writer.append("DisplayName");
-                writer.append(',');
-                writer.append("Age");
-                writer.append('\n');
-
-                writer.append("MKYONG");
-                writer.append(',');
-                writer.append("26");
-                writer.append('\n');
-
-                writer.append("YOUR NAME");
-                writer.append(',');
-                writer.append("29");
-                writer.append('\n');
-                writer.append(texto_do_arquivo);
-                writer.flush();
-                writer.close();
-                Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
-            } catch (IOException e) {
-
-                Toast.makeText(this, "nao", Toast.LENGTH_SHORT).show();
             }
+        });
+        txLongitude = (TextView) findViewById(R.id.txLongitude);
+        txLatitude = (TextView) findViewById(R.id.txLatitude);
+    }
 
+    protected void onResume() {
+        super.onResume();
+        contador_de_passos = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        Sensor acelerometroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+            sensorManager.registerListener(new AcelerometroSensor(), acelerometroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            Toast.makeText(this, "Sensor não encontrado", Toast.LENGTH_SHORT).show();
         }
+    }
 
+    protected void onPause() {
+        super.onPause();
+        contador_de_passos = false;
     }
 
 
+    public void onSensorChanged(SensorEvent event) {
+        if (contador_de_passos) {
+            //textViewCounter.setText(String.valueOf(passos));
+            if (t < event.values[0]) {
+                passos++;
+                textViewCounter.setText(String.valueOf(passos));
+            } else {
+                textViewCounter.setText(String.valueOf(passos));
+            }
+            t = event.values[0];
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "E necessário dar permissões para esta app", Toast.LENGTH_SHORT).show();
+            } else {
+                localionM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                cordenadas = localionM.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+
+                Longitude = cordenadas.getLongitude();
+                Latitude = cordenadas.getLatitude();
+
+                txLatitude.setText("" + Longitude);
+                txLongitude.setText("" + Latitude);
+
+                if (gravar == true) {
+                    try {
+                        texto_do_arquivo = Longitude + ";" + Latitude + ";" + passos;//+";"+event.values[1]+";"+event.values[2]+";"+event.values[3];
+                        writer.append(texto_do_arquivo);
+                        writer.append('\n');
+                    } catch (IOException e) {
+
+                        Toast.makeText(this, "erro na escrita do ficheiro", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    class AcelerometroSensor implements SensorEventListener {
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+
+        }
+
+        public void onSensorChanged(SensorEvent event) {
+            if (gravar == true) {
+                X = event.values[1];
+                Y = event.values[2];
+                Z = event.values[3];
+
+                if(Y < 0) { // O dispositivo esta de cabeça pra baixo
+                    if(X > 0)
+                        textViewDetail.setText("Virando para ESQUERDA ficando INVERTIDO");
+                    if(X < 0)
+                        textViewDetail.setText("Virando para DIREITA ficando INVERTIDO");
+                } else {
+                    if(X > 0)
+                        textViewDetail.setText("Virando para ESQUERDA ");
+                    if(X < 0)
+                        textViewDetail.setText("Virando para DIREITA ");
+                }
+            }
+        }
+    }
 }
